1cc3130f3f1718dfa8c93f62755fd0baf89841ff
Bort <93967399+strudelAndCoffee@users.noreply.github.com>
2022-05-02 23:42:08 -0500

    navbar conditional for current page highlighted made

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -38,16 +38,21 @@ function ContactForm() {
           <form id="contact-form" onSubmit={handleSubmit}>
             <div>
                 <label htmlFor="name">Name:</label>
-                <input type="text" name="name" defaultValue={name} onChange={handleChange} />
+                <input type="text" name="name" defaultValue={name} onBlur={handleChange} />
             </div>
             <div>
                 <label htmlFor="email">Email address:</label>
-                <input type="email" name="email" defaultValue={email} onChange={handleChange} />
+                <input type="email" name="email" defaultValue={email} onBlur={handleChange} />
             </div>
             <div>
                 <label htmlFor="message">Message:</label>
-                <textarea name="message" rows="5" defaultValue={message} onChange={handleChange} />
+                <textarea name="message" rows="5" defaultValue={message} onBlur={handleChange} />
             </div>
+            {errorMessage && (
+                <div>
+                    <p className="error-text">{errorMessage}</p>
+                </div>
+            )}
             <button type="submit">Submit</button>
           </form>
         </section>
